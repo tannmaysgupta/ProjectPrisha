3ead12d0b197cf9b2917801b479c6e1b4f768c55
tannmaysgupta <tanmaygup123@gmail.com>
2017-07-06 03:30:04 +0530

    font decided, new section of HWW

--- mainjs.js
+++ mainjs.js
@@ -1,20 +1,15 @@
 var scrht=window.innerHeight;
-console.log(typeof scrht);
-console.log(scrht);
 var htminnav=window.innerHeight-50;
-console.log(typeof htminnav);
-console.log(htminnav);
 var cc= document.getElementsByClassName('customCarousel')[0];
 var ci= document.getElementsByClassName('carousel-inner')[0];
 cc.style.height=scrht+"px";
 ci.style.height=htminnav+"px";
 
-var a=document.getElementsByTagName('nav')[0];
-
 nav();
 
 function nav()
 {
+var a=document.getElementsByTagName('nav')[0];
 	if(window.pageYOffset>htminnav){
 		a.style.position="fixed";
 		a.style.top=0+"px";
